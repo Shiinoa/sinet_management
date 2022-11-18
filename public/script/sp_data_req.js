456b6b9b02d92acf93ae491c22beef8a8e34d0f6
Shiinoa <max_kkp@hotmail.com>
2022-11-18 10:59:06 +0700

    modified:   public/script/sp_data_req.js

--- public/script/sp_data_req.js
+++ public/script/sp_data_req.js
@@ -1,21 +1,23 @@
 $(document).ready(function(){
     load_data_sp();
     function load_data_sp(){
-        $.ajax({
-            url:"../config/search_sp.php",
-            method:"POST",
-            data:{query:$('#search_data').val()},
-            success:function(data){
-                
-                $('#search_data_result').html(data);
-            }
-        });
-        return false;
+        
+            $.ajax({
+                url:"../config/search_sp.php",
+                method:"POST",
+                data:{query:$('#search_data').val()},
+                success:function(data){
+                    
+                    $('#search_data_result').html(data);
+                }
+            });
+            return false;
+        
        
     }
     $('#search_data').on('keyup',function(){
         var search_sp = $('#search_data').val();
-        console.log(search_sp);
+       
         if(search_sp != ''){
             if(search_sp.length >= 3){
                 load_data_sp();
